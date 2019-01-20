460fe749fd8e33112cfba42226ea34aa59a2b3c6
fellyph <fellyph.cintra@gmail.com>
2019-01-20 20:41:56 +0000

    movind style.css to the project folder and update index.php file with register_style function

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ defined( 'ABSPATH' ) || exit;
  * 1 - Criando nosso primeiro bloco: Parte 02
  *    1.1 - Carregando arquivo externo para nosso estilo
  */
-function meu_primeiro_bloco_gutenberg () {
+function meu_primeiro_bloco_gutenberg_parte_02 () {
 
 	if ( ! function_exists( 'register_block_type' ) ) {
 		// Checamos se temos suporte a função register_block_type antes de tudo.
@@ -22,15 +22,23 @@ function meu_primeiro_bloco_gutenberg () {
 	}
 
 	wp_register_script(
-		'tutorial-gutenberg-01',
+		'tutorial-02',
 		plugins_url( 'primeiro-bloco.js', __FILE__ ),
 		array( 'wp-blocks', 'wp-element' ),
 		filemtime( plugin_dir_path( __FILE__ ) . 'primeiro-bloco.js' )
 	);
 
-	register_block_type( 'fellyph/tutorial-01', array(
-		'editor_script' => 'tutorial-gutenberg-01',
+	wp_register_style(
+		'style-tutorial-02',
+		plugins_url( 'style.css', __FILE__ ),
+		array('wp-edit-blocks'),
+		filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
+	);
+
+	register_block_type( 'fellyph/tutorial-02', array(
+		'editor_script' => 'tutorial-02',
+		'editor_style' => 'style-tutorial-02'
 	) );
 }
 
-add_action( 'init', 'meu_primeiro_bloco_gutenberg' );
+add_action( 'init', 'meu_primeiro_bloco_gutenberg_parte_02' );
